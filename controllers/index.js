0163d38f84acdfdab1f5241e150aa84bc70a9f1a
786-go <noorullahw@gmail.com>
2022-04-07 11:36:32 -0700

    sequlize initialized

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,18 @@
 const router = require("express").Router();
+let db = require("../db/db.sql")
+const fs = require("fs")
+
 
 router.get("/", (req, res) => {
   res.render("../views/testviews.hbs");
 });
+
+
+
+
+
+
+
 
 
 module.exports = router;
--- models/index.js
+++ models/index.js
@@ -1,3 +1,12 @@
+const User = require('./User');
+const Recipes = require('./Recipes');
+
+User.hasMany(Recipes, {
+  foreignKey: 'user_id',
+});
+
+
+module.exports = { User, Recipes };
 
 
 
