5299ef60bf8470743969e36307a2fb16d769be24
Aaron Stokes <stokescomp01@gmail.com>
2022-04-11 00:43:37 -0600

    add recipe submission html, add js for recipe creation

--- public/js/add-recipe.js
+++ public/js/add-recipe.js
@@ -1,25 +1,31 @@
 async function newFormHandler(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const title = document.querySelector('input[name="post-title"]').value;
-    const post_url = document.querySelector('input[name="post-url"]').value;
+  const recipeName = document.querySelector('input[name="name"]').value;
+  const ingredients = document.querySelector('input[name="ingredients"]').value;
+  const post_date = document.querySelector('input[name="post_date"]').value;
+  const directions = document.querySelector('input[name="directions"]').value;
 
-    const response = await fetch(`/api/posts`, {
-        method: 'POST',
-        body: JSON.stringify({
-            title,
-            post_url
-        }),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    });
+  const response = await fetch(`/api/recipes`, {
+    method: "POST",
+    body: JSON.stringify({
+      recipeName,
+      ingredients,
+      post_date,
+      directions,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 
-    if (response.ok) {
-        document.location.replace('/dashboard');
-    } else {
-        alert(response.statusText);
-    }
+  if (response.ok) {
+    document.location.replace("/dashboard");
+  } else {
+    alert(response.statusText);
+  }
 }
 
-document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
+document
+  .querySelector(".new-post-form")
+  .addEventListener("submit", newFormHandler);
