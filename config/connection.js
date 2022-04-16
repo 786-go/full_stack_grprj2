94190f8a9dc56a39d02f3ff238cf61a3a5ef4921
Stephen Carlin <scarlinj@gmail.com>
2022-04-16 16:16:18 -0700

    fixing process.env variables

--- config/connection.js
+++ config/connection.js
@@ -3,16 +3,33 @@ const Sequelize = require("sequelize");
 require("dotenv").config();
 
 // create conneciton to database
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD, {
-    host: "localhost",
-    dialect: "mysql",
-    // mysql port is 3306
-    port: 3306,
-  }
-);
+
+let sequelize;
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD, {
+      host: 'localhost',
+      dialect: 'mysql',
+      // mysql port is 3306
+      port: 3306
+    }
+  );
+}
+
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD, {
+//     host: "localhost",
+//     dialect: "mysql",
+//     // mysql port is 3306
+//     port: 3306,
+//   }
+// );
 
 // create connection to our database, pass in your MySQL information for username and password
 // const sequelize = new Sequelize('just_tech_news_db', 'username', 'password', {
